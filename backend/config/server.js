4182e66b73fd3d5e125c43263add46bf7fc1db42
SotirisKavv <sotos.kavvouras@gmail.com>
2021-10-15 22:18:18 +0300

    created backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0987bf825bf300821e08f1db07304fcd'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6efb7c34-d778-4b7b-bc96-129bfceefa52'
+};
